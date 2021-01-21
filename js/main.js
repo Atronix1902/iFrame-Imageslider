d4e071c3fce5dc86912a94c8200f9cdb1988ec76
Steven-Hendrik Kriebel <49077834+Atronix1902@users.noreply.github.com>
2021-01-22 00:02:08 +0100

    getting parent-directory from url-string

--- js/main.js
+++ js/main.js
@@ -3,7 +3,20 @@ var img0    = document.getElementById('img0');
 var left    = document.getElementById('left');
 var right   = document.getElementById('right');
 
-//Not working yet
+/**
+ * 
+ * @param {string} src source-url
+ * @returns {string} parent-diretory related to src param example: ./test/ -> ./ | https://google.com/test/test/ -> https://google.com/test/ 
+ */
+function getParentDir(src) {
+    let parentDir;
+    if(src.charAt(src.length - 1) === '/') {
+        parentDir = src.substring(0, src.lastIndexOf('/')).substring(0, src.substring(0, src.lastIndexOf('/')).lastIndexOf('/') + 1);
+    } else {
+        parentDir = src.substring(0, src.lastIndexOf('/') + 1);
+    }
+    return parentDir;
+}
 function listFiles(src) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", src);
